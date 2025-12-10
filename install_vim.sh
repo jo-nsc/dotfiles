53cab424cca199557d4f9252d81a1d939cc7be2a
jonsc <jonatasnascimento1@gmail.com>
2025-12-10 12:57:06 +0100

    auto install of plugins to vim

--- install_vim.sh
+++ install_vim.sh
@@ -4,20 +4,25 @@ set -euo pipefail
 REPO_URL="https://github.com/jo-nsc/dotfiles.git"
 DOTDIR="$HOME/.dotfiles"
 
-echo "[1/3] Fetching dotfiles..."
+echo "[1/4] Fetching dotfiles..."
 if [ -d "$DOTDIR/.git" ]; then
   git -C "$DOTDIR" pull --ff-only
 else
   git clone --depth 1 "$REPO_URL" "$DOTDIR"
 fi
 
-echo "[2/3] Installing vimrc..."
+echo "[2/4] Installing vimrc..."
 ln -sf "$DOTDIR/vimrc" "$HOME/.vimrc"
 
-echo "[3/3] Installing vim-plug (if missing)..."
+echo "[3/4] Installing vim-plug (if missing)..."
 if [ ! -f "$HOME/.vim/autoload/plug.vim" ]; then
   curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-echo "Done. Open Vim and run :PlugInstall once."
+echo "[4/4] Installing Vim plugins non-interactively..."
+# --sync waits for install to finish before quitting
+# +qa quits all windows afterward
+vim +'silent! PlugInstall --sync' +qa
+
+echo "All set. Vim is ready."
